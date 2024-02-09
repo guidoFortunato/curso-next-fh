89b74a9f482333cc7a8de8340d7e8b066b27b5cc
Guido <46787485+guidoFortunato@users.noreply.github.com>
2024-02-09 01:22:16 -0300

    cambios

---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prisma";
+import { TodosGrid } from "@/todos/components/TodosGrid";
+
+export const metadata = {
+ title: 'Listado de Todos',
+ description: 'SEO Title',
+};
+
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+
+  return (
+    <div>
+      <TodosGrid todos={todos} />
+    </div>
+  );
+}
